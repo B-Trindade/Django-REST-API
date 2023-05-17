92960616db8b4369ca63639cfcf5b8deb13ef1b4
Breno Tostes <tostes@pop-os.localdomain>
2023-05-17 17:42:48 -0300

    Fixed linting errors2

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -28,4 +28,3 @@ class Command(BaseCommand):
                 )
                 time.sleep(1)
         self.stdout.write(self.style.SUCCESS('Database available!'))
-        
--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -28,9 +28,9 @@ class CommandTests(SimpleTestCase):
     def test_wait_for_db_delay(self, patched_sleep, patched_check):
         """Test waiting for database when getting OperationalError."""
 
-        #Psycopg2Error usually happens when PostgreSQL 
+        # Psycopg2Error usually happens when PostgreSQL
         # is not ready to accept connections
-        #OperationalError happens when the database is ready but
+        # OperationalError happens when the database is ready but
         # the testing database has not been created yet (raised by Django)
 
         patched_check.side_effect = [Psycopg2Error] * 2 + \
@@ -38,8 +38,8 @@ class CommandTests(SimpleTestCase):
 
         call_command('wait_for_db')
 
-        # The first 2 times are expected to raise Psycopg2Error and 
-        #  the following 3 times should raise OpErr (in 6th we get OK)
+        # The first 2 times are expected to raise Psycopg2Error and
+        # the following 3 times should raise OpErr (in 6th we get OK)
         self.assertEqual(patched_check.call_count, 6)
         patched_check.assert_called_with(databases=['default'])
 
