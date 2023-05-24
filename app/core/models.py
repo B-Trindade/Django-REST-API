8130625b4eea016071f6b7255764f72fda089dfc
Breno Tostes <tostes@pop-os.localdomain>
2023-05-24 19:12:00 -0300

    Implemented Recipe model and update migartions

--- app/core/models.py
+++ app/core/models.py
@@ -4,6 +4,7 @@ Each model corresponds to a class in the file and
 is related to a table.
 """
 
+from django.conf import settings
 from django.db import models
 from django.contrib.auth.models import (
     AbstractBaseUser,
@@ -49,3 +50,22 @@ class User(AbstractBaseUser, PermissionsMixin):
     objects = UserManager()
 
     USERNAME_FIELD = 'email'
+
+
+class Recipe(models.Model):
+    """Recipe object."""
+
+    # We use the ForeignKey to allow us to stablish a relationship
+    # between models, in this case Recipe model and the User model.
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+    )
+    title = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    time_minutes = models.IntegerField()
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    link = models.CharField(max_length=255, blank=True)
+
+    def __str__(self):
+        return self.title
